3a51c57546e1b96d26ea65b611bcc100714f79fb
jiangwei1 <jiangwei1@kylinos.com>
2020-12-09 20:57:28 +0800

    个人主页基本样式完成

--- bbs/blueprint/profile.py
+++ bbs/blueprint/profile.py
@@ -6,13 +6,29 @@
 @File    : profile.py
 @Software: PyCharm
 """
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
-from bbs.models import User
+from bbs.models import User, Comments, Post
+
 profile_bp = Blueprint('profile', __name__, url_prefix='/profile')
 
 
 @profile_bp.route('/user/<user_id>/')
+@profile_bp.route('/posts/<user_id>/')
 def index(user_id):
+    print(user_id)
     user = User.query.get_or_404(user_id)
-    return render_template('frontend/profile.html', user=user)
+    pagination = Post.query.filter(Post.author_id == user_id, Post.status_id == 1).order_by(
+        Post.create_time.desc()).paginate()
+    posts = pagination.items
+    return render_template('frontend/profile.html', user=user, pagination=pagination, posts=posts)
+
+
+@profile_bp.route('/comment/<user_id>/')
+def profile_comment(user_id):
+    page = request.args.get('page', default=1, type=int)
+    user = User.query.get_or_404(user_id)
+    pagination = Comments.query.filter(Comments.author_id == user.id, Comments.delete_flag == 0).order_by(
+        Comments.timestamps.desc()).paginate(page=page, per_page=20)
+    comments = pagination.items
+    return render_template('frontend/profile-comment.html', user=user, comments=comments)
